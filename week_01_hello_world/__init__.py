29e02340ae1544953e14bca165d7db90aadbe6dd
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-17 20:26:24 +0800

    urhgiuhhgr

--- week_01_hello_world/__init__.py
+++ week_01_hello_world/__init__.py
@@ -4,6 +4,14 @@ import datetime
 name: str = input("What is your name? ")
 age: int = int(input("What is your age? "))
 birth_year: int = datetime.date.today().year - age
+future_age: int = age + 10
+fav_color: str = input("What is your favorite color? ")
+
+
+
 
 print(f"Hello, {name}! You are {age} years old.")
 print(f"You were born around the year {birth_year}.")
+print(f"In 10 years you will be {future_age} years old.")
+
+
