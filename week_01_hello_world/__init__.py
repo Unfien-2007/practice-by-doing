6a224709b85f3da041f3d8ee114062fc48e6751d
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-17 20:26:40 +0800

    jgcgcg

--- week_01_hello_world/__init__.py
+++ week_01_hello_world/__init__.py
@@ -7,11 +7,6 @@ birth_year: int = datetime.date.today().year - age
 future_age: int = age + 10
 fav_color: str = input("What is your favorite color? ")
 
-
-
-
 print(f"Hello, {name}! You are {age} years old.")
 print(f"You were born around the year {birth_year}.")
 print(f"In 10 years you will be {future_age} years old.")
-
-
