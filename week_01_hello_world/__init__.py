eb7dc99c9d247f218fc6f712a655dc1fffe671ad
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-18 18:37:16 +0800

    oihfoih

--- week_01_hello_world/__init__.py
+++ week_01_hello_world/__init__.py
@@ -8,7 +8,8 @@ future_age: int = age + 10
 fav_color: str = input("What is your favorite color? ")
 months_old: int = age * 12
 
-print(f"Hello, {name}! You are {age} years old.")
+print(f"Hello, {name}! You are {age} years old and your favorite color is {fav_color}.")
 print(f"You were born around the year {birth_year}.")
 print(f"In 10 years you will be {future_age} years old.")
-print(f"Your favorite color is {fav_color}.")
+print(f"You are approximately {months_old} months old.")
+
