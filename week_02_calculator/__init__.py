5591574b36665a3a5d889a81a2e7cf69a91be668
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-29 19:28:44 +0800

    roiuhturh

--- week_02_calculator/__init__.py
+++ week_02_calculator/__init__.py
@@ -1,23 +1,23 @@
 #shoud be done to practice functions and modules in python
 
-num1 = int(input("Enter First Number:"))
-num2 = int(input("Enter Second Number:"))
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
 
 def addition():
     result = num1 + num2
-    return print(result)
+    return print(f"{num1} Plus {num2} = {result}")
 
 def substraction():
     result = num1 - num2
-    return print(result)
+    return print(f"{num1} Minus {num2} = {result}")
 
 def multiplication():
     result = num1 * num2
-    return print(result)
+    return print(f"{num1} Times {num2} = {result}")
 
 def devision():
     result = num1 / num2
-    return print(result)
+    return print(f"{num1} Devide {num2} = {result}")
 
 addition()
 substraction()
