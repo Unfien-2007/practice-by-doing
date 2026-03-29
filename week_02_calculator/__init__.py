007d32a8c9c68c5c2fb10ca53b13ba69b922ae0e
[Unfien-2007] <Unfienproductions@gmail.com>
2026-03-29 19:26:00 +0800

    yuaegfuvaeufvef

--- week_02_calculator/__init__.py
+++ week_02_calculator/__init__.py
@@ -5,4 +5,21 @@ num2 = int(input("Enter Second Number:"))
 
 def addition():
     result = num1 + num2
-    return result
+    return print(result)
+
+def substraction():
+    result = num1 - num2
+    return print(result)
+
+def multiplication():
+    result = num1 * num2
+    return print(result)
+
+def devision():
+    result = num1 / num2
+    return print(result)
+
+addition()
+substraction()
+multiplication()
+devision()
