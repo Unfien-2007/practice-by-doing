9895d3563d825960b47b8b2b08823c99edddf6d3
[Unfien-2007] <Unfienproductions@gmail.com>
2026-04-01 19:01:40 +0800

    uugigg

--- week_03_guessing_game/__init__.py
+++ week_03_guessing_game/__init__.py
@@ -1,14 +1,19 @@
 #random guese game
-
 import random as rd
-
+answer = rd.randrange(1,99)
 guess = int(input("Input Guess(1-99): "))
 
 #logic 
 
-answer = rd.randrange(1,99)
+
 
 if answer == guess :
     print(f"Your Guess {guess} is the right answer")
+elif answer <= guess:
+    print(f"Your Guess {guess} is Lower")
+elif answer >= guess & answer < 100:
+    print(f"Your Guess {guess} is higher")
+else:
+    print(f"Your Guess {guess} should be 1-99")
 
 
