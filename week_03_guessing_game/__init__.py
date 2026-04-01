f08f3be8a389d664f1989753067d0682652df041
[Unfien-2007] <Unfienproductions@gmail.com>
2026-04-01 19:05:37 +0800

    fguffgef

--- week_03_guessing_game/__init__.py
+++ week_03_guessing_game/__init__.py
@@ -6,14 +6,15 @@ guess = int(input("Input Guess(1-99): "))
 #logic 
 
 
+while answer != guess:
+    if answer == guess :
+        print(f"Your Guess {guess} is the right answer")
+    elif answer <= guess:
+        print(f"Your Guess {guess} is Lower")
+    elif answer >= guess & answer < 100:
+        print(f"Your Guess {guess} is higher")
+    else:
+        print(f"Your Guess {guess} should be 1-99")
 
-if answer == guess :
-    print(f"Your Guess {guess} is the right answer")
-elif answer <= guess:
-    print(f"Your Guess {guess} is Lower")
-elif answer >= guess & answer < 100:
-    print(f"Your Guess {guess} is higher")
-else:
-    print(f"Your Guess {guess} should be 1-99")
 
 
