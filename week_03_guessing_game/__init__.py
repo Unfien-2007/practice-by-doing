85c7ba57495feac07045f1ebf71c8609fafdd297
[Unfien-2007] <Unfienproductions@gmail.com>
2026-04-01 18:54:14 +0800

    dgfyghfghghfd

--- week_03_guessing_game/__init__.py
+++ week_03_guessing_game/__init__.py
@@ -2,14 +2,13 @@
 
 import random as rd
 
-
 guess = int(input("Input Guess(1-99): "))
 
 #logic 
 
 answer = rd.randrange(1,99)
 
-
-if answer = guess :
+if answer == guess :
+    print(f"Your Guess {guess} is the right answer")
 
 
