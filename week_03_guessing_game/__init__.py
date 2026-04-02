46b59a95383e47bd5e22ceb6f0768f1f07508a5a
[Unfien-2007] <Unfienproductions@gmail.com>
2026-04-02 21:06:49 +0800

    ehbsbfbbbdnbns

--- week_03_guessing_game/__init__.py
+++ week_03_guessing_game/__init__.py
@@ -1,20 +1,24 @@
 #random guese game
 import random as rd
 answer = rd.randrange(1,99)
-guess = int(input("Input Guess(1-99): "))
 
 #logic 
-
-
-while answer != guess:
-    if answer == guess :
+if answer == guess :
         print(f"Your Guess {guess} is the right answer")
-    elif answer <= guess:
+elif answer <= guess:
         print(f"Your Guess {guess} is Lower")
     elif answer >= guess & answer < 100:
         print(f"Your Guess {guess} is higher")
     else:
         print(f"Your Guess {guess} should be 1-99")
 
+
+while answer != guess:
+guess = int(input("Input Guess(1-99): "))
+
+    
+
+
+#error to fix program ran infinite
 
 
