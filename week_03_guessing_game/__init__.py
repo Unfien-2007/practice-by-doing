9c6385c995c822e6517c54f68c98484c182da94a
[Unfien-2007] <Unfienproductions@gmail.com>
2026-04-02 21:17:03 +0800

    hwjjrvvwjvre

--- week_03_guessing_game/__init__.py
+++ week_03_guessing_game/__init__.py
@@ -1,29 +1,24 @@
-#random guese game
+# random guess game
 import random as rd
-answer = rd.randrange(1,99)
+
+answer = rd.randint(1, 99)
 
 won = False
-while won == False:
-    guess = int(input("Input Guess(1-99): "))
+while not won:
+    try:
+        guess = int(input("Input Guess (1-99): "))
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
 
-
-#logic 
-if answer == guess :
-    print(f"Your Guess {guess} is the right answer")
-    won = True
-elif answer <= guess:
-    print(f"Your Guess {guess} is Lower")
-elif answer >= guess & answer < 100:
-    print(f"Your Guess {guess} is higher")
-else:
-    print(f"Your Guess {guess} should be 1-99")
-
-
-
-
-    
-
-
-#error to fix program ran infinite
+    if guess < 1 or guess > 99:
+        print(f"Your Guess {guess} should be 1-99")
+    elif answer == guess:
+        print(f"Your Guess {guess} is the right answer")
+        won = True
+    elif guess > answer:
+        print(f"Your Guess {guess} is Lower")
+    else:
+        print(f"Your Guess {guess} is Higher")
 
 
